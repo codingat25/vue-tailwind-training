a89cc6602e50808409a20531a6f1f6e5da1854ae
ChristianReyes <christianquimporeyes@gmail.com>
2021-09-21 23:19:06 +0800

    practice

--- src/components/HeroBanner.vue
+++ src/components/HeroBanner.vue
@@ -1,5 +1,5 @@
 <template>
-    <div class="relative bg-hero-background-mobile sm:bg-hero-background bg-cover bg-center sm:bg-right h-screen w-full">
+    <div class="relative bg-hero-background-mobile sm:bg-hero-background bg-cover bg-center sm:bg-center h-screen w-full">
         
     </div>
 </template>
